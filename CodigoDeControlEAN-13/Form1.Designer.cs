2a9ab7356debc0b573e32893b36d9bbc6282175d
DanyFlores <42879328+DanyFlores@users.noreply.github.com>
2021-04-21 13:01:01 -0500

    Initial commit

---
+++ CodigoDeControlEAN-13/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿
+namespace CodigoDeControlEAN_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtCodigiControl = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.Dia = new System.Windows.Forms.Label();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtAño = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodigoBarras = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigo.Location = new System.Drawing.Point(49, 40);
+            this.txtCodigo.MaxLength = 300;
+            this.txtCodigo.Multiline = true;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(390, 28);
+            this.txtCodigo.TabIndex = 0;
+            // 
+            // txtCodigiControl
+            // 
+            this.txtCodigiControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigiControl.Location = new System.Drawing.Point(235, 126);
+            this.txtCodigiControl.Multiline = true;
+            this.txtCodigiControl.Name = "txtCodigiControl";
+            this.txtCodigiControl.Size = new System.Drawing.Size(84, 30);
+            this.txtCodigiControl.TabIndex = 2;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(364, 74);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 35);
+            this.btnGenerar.TabIndex = 3;
+            this.btnGenerar.Text = "Generar  ";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ingrese codigo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Codigo de Barras";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(503, 241);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.txtCodigoBarras);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.txtCodigo);
+            this.tabPage1.Controls.Add(this.txtCodigiControl);
+            this.tabPage1.Controls.Add(this.btnGenerar);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(495, 215);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "EAN13";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.btnCalcular);
+            this.tabPage2.Controls.Add(this.txtResultado);
+            this.tabPage2.Controls.Add(this.txtAño);
+            this.tabPage2.Controls.Add(this.txtMes);
+            this.tabPage2.Controls.Add(this.Dia);
+            this.tabPage2.Controls.Add(this.txtDia);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(495, 215);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Calendario";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Dia
+            // 
+            this.Dia.AutoSize = true;
+            this.Dia.Location = new System.Drawing.Point(100, 29);
+            this.Dia.Name = "Dia";
+            this.Dia.Size = new System.Drawing.Size(23, 13);
+            this.Dia.TabIndex = 6;
+            this.Dia.Text = "Dia";
+            // 
+            // txtDia
+            // 
+            this.txtDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDia.Location = new System.Drawing.Point(103, 45);
+            this.txtDia.MaxLength = 300;
+            this.txtDia.Multiline = true;
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(67, 28);
+            this.txtDia.TabIndex = 5;
+            // 
+            // txtMes
+            // 
+            this.txtMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMes.Location = new System.Drawing.Point(176, 45);
+            this.txtMes.MaxLength = 300;
+            this.txtMes.Multiline = true;
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(67, 28);
+            this.txtMes.TabIndex = 7;
+            // 
+            // txtAño
+            // 
+            this.txtAño.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAño.Location = new System.Drawing.Point(249, 45);
+            this.txtAño.MaxLength = 300;
+            this.txtAño.Multiline = true;
+            this.txtAño.Name = "txtAño";
+            this.txtAño.Size = new System.Drawing.Size(67, 28);
+            this.txtAño.TabIndex = 8;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(103, 105);
+            this.txtResultado.MaxLength = 300;
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(213, 28);
+            this.txtResultado.TabIndex = 9;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(341, 45);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 28);
+            this.btnCalcular.TabIndex = 10;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(176, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Mes";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(246, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Año";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Dias Transcurridos";
+            // 
+            // txtCodigoBarras
+            // 
+            this.txtCodigoBarras.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigoBarras.Location = new System.Drawing.Point(49, 126);
+            this.txtCodigoBarras.Multiline = true;
+            this.txtCodigoBarras.Name = "txtCodigoBarras";
+            this.txtCodigoBarras.Size = new System.Drawing.Size(165, 30);
+            this.txtCodigoBarras.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(232, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Digito de control ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 245);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtCodigiControl;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label Dia;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.TextBox txtAño;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCodigoBarras;
+    }
+}
+
---
+++ CodigoDeControlEAN-13/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodigoDeControlEAN_13
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region Ean13
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtCodigoBarras.Text = txtCodigo.Text;
+                txtCodigiControl.Text = Convert.ToString(ObtenerDigitoVerificadorEAN13(txtCodigo.Text));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int ObtenerDigitoVerificadorEAN13(string Codigo)
+        {
+            if (Codigo.Length == 12)
+            {
+                int suma = 0;
+                //Recorrer los caracteres del código de barras
+                for (int i = 0; i < Codigo.Length; i++)
+                {
+                    //Verificar que el caracter sea un número
+                    if (Char.IsDigit(Codigo[i]))
+                    {
+                        //Convertir el caracter a entero
+                        int valor = int.Parse(Codigo[i].ToString());
+                        //Identificar el valor del corrector (La posición es par o impar)
+                        //suma += ((i + 1) % 2 == 0) ? (valor * 3) : valor; // esta forma igual funciona 
+                        suma += ((13 - i) % 2 == 0) ? (valor * 3) : valor;  
+                    }
+                    else
+                    {
+                        throw new Exception("Caracter inválido");
+                    }
+                }
+                
+                int sobrante = suma % 10; // obtenemos el residuo (el sobrante nunca es mayor a 9)
+                int digitoVerificador = 0; 
+                if (sobrante > 0) 
+                    digitoVerificador = 10 - sobrante; // a 10 le restamos el residuo para obtener el digito de control
+                return digitoVerificador;
+            }
+            else
+            {
+                throw new Exception("La longitud del código EAN13 debe ser de 12 caracteres");
+            }
+        }
+
+        #endregion
+
+        #region Calendario
+        private static readonly Dictionary<int, int> mesesDictionary = new Dictionary<int, int>() {
+        {1, 31},
+        {2, 28},
+        {3, 31},
+        {4, 30},
+        {5, 31},
+        {6, 30},
+        {7, 31},
+        {8, 31},
+        {9, 30},
+        {10, 31},
+        {11, 30},
+        {12, 31}
+        };
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int d, m, a;
+                d = int.Parse(txtDia.Text);
+                m = int.Parse(txtMes.Text);
+                a = int.Parse(txtAño.Text);
+                int diastrascurridos = DiasTranscurridos(d,m,a);
+                txtResultado.Text = diastrascurridos.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+               
+        private const int BaseYear = 2000;
+        private const int DiasBisiesto = 366;
+        private const int DiasNoBisiesto = 365;
+        public int DiasTranscurridos(int d, int m, int a)
+        {
+            //Identificar si el año es bisiesto;
+            var bisiesto = EsBisiesto(a);
+            //Identificar el día máximo por mes;
+            var maxDia = mesesDictionary[m];
+            //Si es febrero, y a la vez el año es bisiesto, sumar uno
+            if (m == 2 && bisiesto)
+                maxDia++;
+            //Validaciones
+            if (a < BaseYear)
+                throw new Exception("Ingrese una fecha mayor o igual a 01/Enero/2000");
+            if (m < 1 || m > 12)
+                throw new Exception("Ingrese una fecha válida (mes no válido)");
+            if (d < 1 || d > maxDia)
+                throw new Exception("Ingrese una fecha válida (día no válido)");
+            int diasTranscurridos = DiasAñosCompletos(a) + DiasUltimoAño(d, m, a);
+            return diasTranscurridos - 1;
+        }
+        private bool EsBisiesto(int a)
+        {
+            if (a % 4 == 0 && (a % 100 != 0 || a % 400 == 0))
+                return true;
+            return false;
+        }
+        private int DiasAñosCompletos(int a)
+        {
+            int dias = 0;
+            for (int i = BaseYear; i < a; i++)
+            {
+                dias += EsBisiesto(i) ? DiasBisiesto : DiasNoBisiesto;
+            }
+            return dias;
+        }
+        private int DiasUltimoAño(int d, int m, int a)
+        {
+            int dias = 0;
+            //Desde el mes de enero, ir sumando los días
+            for (int i = 1; i < m; i++)
+            {
+                var aux = mesesDictionary[i];
+                dias += (i == 2 && EsBisiesto(a)) ? aux + 1 : aux;
+            }
+            dias += d;
+            return dias;
+        }
+
+        #endregion
+    }
+}
